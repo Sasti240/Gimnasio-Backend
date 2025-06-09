0574a3dfac4b902455ccd4c076b44197432726c5
Santiago <sastijim2010@gmail.com>
2025-06-08 19:39:34 -0500

    Preparación despliegue

--- src/main/java/com/gimnasio/gimnasio_backend/config/CorsConfig.java
+++ src/main/java/com/gimnasio/gimnasio_backend/config/CorsConfig.java
@@ -14,7 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") 
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000") 
+                        .allowedOrigins("http://localhost:5173", 
+                        "http://localhost:3000",
+                        "https://gimnasio-frontend.onrender.com"
+                        )
+
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
                         .allowedHeaders("*") 
                         .allowCredentials(true); 
